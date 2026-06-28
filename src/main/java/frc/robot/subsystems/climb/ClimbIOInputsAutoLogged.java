940a3a6441957fae5125bddd2010f944c220eea3
bhlcusd <242719234+bhlcusd@users.noreply.github.com>
2026-06-28 16:19:50 -0700

    update autologged class

--- src/main/java/frc/robot/subsystems/climb/ClimbIOInputsAutoLogged.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOInputsAutoLogged.java
@@ -6,28 +6,28 @@ import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 public class ClimbIOInputsAutoLogged extends ClimbIO.ClimbIOInputs implements LoggableInputs, Cloneable {
-    @Override
-    public void toLog(LogTable table) {
-        table.put("Volts", volts);
-        table.put("Amps", amps);
-        table.put("Pos", pos);
-        table.put("Vel", vel);
-    }
+  @Override
+  public void toLog(LogTable table) {
+    table.put("Voltage_v", voltage_v);
+    table.put("Current_a", current_a);
+    table.put("Pos_deg", pos_deg);
+    table.put("Vel_dps", vel_dps);
+  }
 
-    @Override
-    public void fromLog(LogTable table) {
-        volts = table.get("Volts", volts);
-        amps = table.get("Amps", amps);
-        pos = table.get("Pos", pos);
-        vel = table.get("Vel", vel);
-    }
+  @Override
+  public void fromLog(LogTable table) {
+    voltage_v = table.get("Voltage_v", voltage_v);
+    current_a = table.get("Current_a", current_a);
+    pos_deg = table.get("Pos_deg", pos_deg);
+    vel_dps = table.get("Vel_dps", vel_dps);
+  }
 
-    public ClimbIOInputsAutoLogged clone() {
-        ClimbIOInputsAutoLogged copy = new ClimbIOInputsAutoLogged();
-        copy.volts = this.volts;
-        copy.amps = this.amps;
-        copy.pos = this.pos;
-        copy.vel = this.vel;
-        return copy;
-    }
+  public ClimbIOInputsAutoLogged clone() {
+    ClimbIOInputsAutoLogged copy = new ClimbIOInputsAutoLogged();
+    copy.voltage_v = this.voltage_v;
+    copy.current_a = this.current_a;
+    copy.pos_deg = this.pos_deg;
+    copy.vel_dps = this.vel_dps;
+    return copy;
+  }
 }
